5f80adf450eb81c93bcc3b254a4c8ccf89e6b8ac
Sakshi Shah <sakshi.shah@simformsolutions.com>
2025-03-25 17:00:55 +0530

    feature: update with advance functions

--- script/main.js
+++ script/main.js
@@ -29,6 +29,23 @@ const initializeButtons = (buttonClass) => {
     document.querySelector('.clear-btn').addEventListener('click', () => calculator.clearDisplay());
     document.querySelector('.pi-btn').addEventListener('click', () => calculator.appendPi());
 
+    //Advance Functions
+    document.querySelector('.modulus-btn').addEventListener('click', () => calculator.appendValue('%'));
+    document.querySelector('.exponent-btn').addEventListener('click', () => calculator.appendValue('^'));
+    document.querySelector('.factorial-btn').addEventListener('click', () => calculator.appendValue('!'));
+    document.querySelector('.log-btn').addEventListener('click', () => calculator.log());
+    //document.querySelector('.eulars-btn').addEventListener('click', () => this.eulersFormula());
+    document.querySelector('.logn-btn').addEventListener('click', () => calculator.ln());
+    document.querySelector('.reciprocal-btn').addEventListener('click', () => calculator.reciprocal());
+    document.querySelector('.abs-btn').addEventListener('click', () => calculator.absoluteValue());
+    document.querySelector('.square-btn').addEventListener('click', () => calculator.square());
+    document.querySelector('.sqrt-btn').addEventListener('click', () => calculator.sqrt());
+    document.querySelector('.power-btn').addEventListener('click', () => calculator.xpowery());
+    document.querySelector('.ten-power-btn').addEventListener('click', () => calculator.tenPowerX());
+    //document.querySelector('.toggle-sign-btn').addEventListener('click', () => calculator.toggleSign());
+    //document.querySelector('.second-btn').addEventListener('click', (e) => calculator.toggleSecondPrimary(e.target));
+    document.querySelector('.fe-btn').addEventListener('click', () => calculator.FEmode());
+
     // Keyboard input
     document.addEventListener('keydown', handleKeyPress.bind(calculator));
 };
