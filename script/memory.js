63e1b73d779a96f717e902fc6ebc08a9027fb618
Sakshi Shah <sakshi.shah@simformsolutions.com>
2025-03-26 10:40:20 +0530

    feature: add memory file to store memory functions

---
+++ script/memory.js
@@ -0,0 +1,63 @@
+export function handleMC() {
+    if (localStorage) {
+        localStorage.removeItem('calculationOutput');
+        console.log("Memory Cleared");
+    } else {
+        console.error("Error clearing memory: localStorage is not available.");
+    }
+}
+
+export function handleMR(screen) {
+    let inputvalue = screen.textContent
+    if (localStorage) {
+        const memoryValue = localStorage.getItem('calculationOutput');
+        if (memoryValue) {
+            screen.textContent = inputvalue.concat(memoryValue);
+        } else {
+            console.log("No memory value stored.");
+        }
+    } else {
+        console.error("Error recalling memory: localStorage is not available.");
+    }
+}
+
+export function handleMS(screen, getValueCallback) {
+    if (localStorage) {
+        const valueToStore = getValueCallback(screen);
+        if (valueToStore !== null && valueToStore !== undefined) {
+            localStorage.setItem('calculationOutput', valueToStore);
+            console.log(`Memory Stored: ${valueToStore}`);
+        } else {
+            console.error("Error storing memory: Invalid value.");
+        }
+    } else {
+        console.error("Error storing memory: localStorage is not available.");
+    }
+}
+
+export function handleMplusAndMinus(ref, screen, getValueCallback) {
+    if (localStorage) {
+        const memoryValue = parseFloat(localStorage.getItem('calculationOutput') || "0");
+        const currentValue = parseFloat(getValueCallback(screen));
+
+        if (isNaN(memoryValue) || isNaN(currentValue)) {
+            console.error("Error performing memory operation: Invalid number value.");
+            return;
+        }
+
+        let newValue;
+        if (ref.className.includes('plus')) {
+            newValue = memoryValue + currentValue;
+        } else if (ref.className.includes('minus')) {
+            newValue = memoryValue - currentValue;
+        } else {
+            console.error("Error performing memory operation: Invalid memory operation.");
+            return;
+        }
+
+        localStorage.setItem('calculationOutput', newValue.toString());
+        console.log(`New Memory Value: ${newValue}`);
+    } else {
+        console.error("Error performing memory operation: localStorage is not available.");
+    }
+}
